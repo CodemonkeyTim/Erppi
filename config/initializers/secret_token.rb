c6518f74a4f8552436a9bffddb2ff1f357b5f534
CodemonkeyTim <tim@silvertreksystems.com>
2012-10-02 11:32:18 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Erppi::Application.config.secret_token = '128e1c0af26858435e2b01e4cf01523a3938da715acd7d16217f24f4427441c89f2cc6631100eb967da713aec8fedd1e0eb570b2a2061dca4dc860464af20434'
